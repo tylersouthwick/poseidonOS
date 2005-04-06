fc91f99e042d1b712f2222c9364abf3a4219c507
Tyler Southwick <tyler@northfuse.net>
2005-04-06 23:59:32 +0000

    devices.h - adds support for generic device functions, using the device_map as a lookup table kdebug.h - list of debug switches to turn off or on
    git-svn-id: https://northfuse.net/svn/PoseidonOS/trunk@756 0e8f351e-ab8b-4634-bd77-693fb702483a

---
+++ poseidonos/kernel/include/devices.h
@@ -0,0 +1,7 @@
+#ifndef DEVICE_H
+#define DEVICE_H
+
+#include <floppy.h>
+
+#endif
+
---
+++ poseidonos/kernel/include/kdebug.h
@@ -0,0 +1,9 @@
+#ifndef KDEBUG_H
+#define KDEBUG_H
+
+//#define DEBUG_MM
+//#define DEBUG_MULTITASKING
+//#define DEBUG_DEVICEMANAGER
+
+#endif
+
