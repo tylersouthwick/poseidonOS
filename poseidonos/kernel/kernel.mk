76867f183c72bae8eb96bb123ffe4143e29e30da
Tyler Southwick <tyler@northfuse.net>
2005-05-10 13:55:09 +0000

    *** empty log message ***
    git-svn-id: https://northfuse.net/svn/PoseidonOS/trunk@794 0e8f351e-ab8b-4634-bd77-693fb702483a

--- poseidonos/kernel/kernel.mk
+++ poseidonos/kernel/kernel.mk
@@ -1,36 +1,51 @@
+#####################################
+## Variables
+## change the POSEIDON_ROOT to match your setup
+####################################
 POSEIDON_ROOT=/home/northfuse/workspace/poseidonos
-SRC_ROOT=${POSEIDON_ROOT}
-BUILD_ROOT=${POSEIDON_ROOT}/build
-DOC_ROOT=${POSEIDON_ROOT}/doc
 
-KERNEL_ROOT=${SRC_ROOT}/kernel
-DRIVERS_ROOT=${KERNEL_ROOT}/drivers
-UTIL_ROOT=${KERNEL_ROOT}/util
-
-AS_FORMAT=elf
-
-CC = gcc
 AS = nasm
-
+AS_FORMAT=elf
+CC = gcc
 AR = ar
 ARFLAGS = -rs
 
-KERNEL_INCLUDE=${KERNEL_ROOT}/include
-KERNEL_INCLUDE_DIR=-I${KERNEL_INCLUDE}
-DRIVER_INCLUDE_DIR=-I${DRIVERS_ROOT}/include
+KERNEL_CFLAGS = -ffreestanding -nostdlib -fno-builtins -Wall -Winline -Wstrict-prototypes -Wmissing-prototypes -Wmissing-declarations -Wredundant-decls -finline-functions -fpic 
 
-KERNEL_CFLAGS = -ffreestanding -nostdlib -fno-builtins -Wall -Winline -Wstrict-prototypes -Wmissing-prototypes -Wmissing-declarations -Wredundant-decls -finline-functions  
+#####################################
+## DON't EDIT BELOW THIS LINE
+## unless you know what you're doing
+#####################################
 
-CFLAGS = ${KERNEL_CFLAGS} ${KERNEL_INCLUDE_DIR} ${DRIVER_INCLUDE_DIR}
+################
+## build variables
+################
+SRC_ROOT=$(POSEIDON_ROOT)
+KERNEL_ROOT=$(SRC_ROOT)/kernel
+KERNEL_INCLUDE=$(KERNEL_ROOT)/include
 
-compile : ${LIB_NAME}.a
-${LIB_NAME}.a: $(C_OBJS) ${ASM_OBJS}
-	$(AR) $(ARFLAGS) ${LIB_NAME}.a $(C_OBJS) ${ASM_OBJS}
+CFLAGS = $(KERNEL_CFLAGS) -I$(KERNEL_INCLUDE)
 
-#looking for a dependency that wasn't already found?
+###############
+## targets
+###############
+
+######
+# build library
+######
+compile : $(LIB_NAME).a
+$(LIB_NAME).a: $(C_OBJS) $(ASM_OBJS)
+	$(AR) $(ARFLAGS) $(LIB_NAME).a $(C_OBJS) $(ASM_OBJS)
+
+######
+# assembly dependencies
+######
 %.o : %.asm
-	${AS} -f ${AS_FORMAT} -o $@ $?
+	$(AS) -f $(AS_FORMAT) -o $@ $?
 
+######
+# generate dependencies
+######
 %.d : %.c
 	@set -e; rm -f $@; \
 		$(CC) -M $(CFLAGS) $< > $@.$$$$; \
@@ -41,8 +56,14 @@ C_SRCS=$(subst .o,.c,$(C_OBJS))
 MAKEFILES_D=$(subst .c,.d,$(C_SRCS))
 include $(MAKEFILES_D)
 
+######
+# clean up temporary files
+######
 clean:
-	rm -f ${C_OBJS} ${ASM_OBJS} ${LIB_NAME}.a 
+	rm -f $(C_OBJS) $(ASM_OBJS) $(LIB_NAME).a 
 
+######
+# clean up ALL temporary files
+######
 depclean : clean
 	rm -f $(MAKEFILES_D)
