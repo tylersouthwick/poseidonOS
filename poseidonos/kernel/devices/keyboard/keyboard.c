81285011f5ab937fbe61ecfa1494bf2546d96b64
Tyler Southwick <tyler@northfuse.net>
2005-05-16 02:26:47 +0000

    *** empty log message ***
    git-svn-id: https://northfuse.net/svn/PoseidonOS/trunk@797 0e8f351e-ab8b-4634-bd77-693fb702483a

--- poseidonos/kernel/devices/keyboard/keyboard.c
+++ poseidonos/kernel/devices/keyboard/keyboard.c
@@ -23,21 +23,51 @@ static unsigned int is_done;
 extern void keyboard_isr(void);
 
 static char scancodes[] = KEYBOARD_QWERTY;
+static char caps_scancodes[] = KEYBOARD_QWERTY_CAPS;
 
 void keyboard_init() {
 	idt_interrupt_add(0x21, keyboard_isr, 0);
 	irq_umask(IRQ_1);
 
 	/*initilize keyboard buffer*/
-	keyboard_buffer = kmalloc(KEYBOARD_BUFFER_SIZE);
+	keyboard_buffer = 0;
 	keyboard_buffer_index = 0;
 }
 
+static bool is_shifted;
+static bool is_capslocked;
+
 static char convert_scancode(int scancode)
 {
+	char ch;
+
 	if (scancode >= sizeof(scancodes))
 		return 0;
-	return scancodes[scancode];
+
+	ch = scancodes[scancode];
+
+	if (ch == KEY_SHIFT)
+	{
+		is_shifted = true;
+		return 0;
+	}
+
+	if (ch == KEY_CAPSLOCK)
+	{
+		is_capslocked = (is_capslocked) ? false : true;
+
+		return 0;
+	}
+
+	if (is_shifted || is_capslocked)
+	{
+		ch = caps_scancodes[scancode];
+		is_shifted = false;
+	}
+
+	is_shifted = false;
+
+	return ch;
 }
 
 void keyboard_irq() {
@@ -48,20 +78,34 @@ void keyboard_irq() {
 	status = inportb(0x64);
 	scancode = inportb(0x60);
 	ch = convert_scancode(scancode);
+
+	kprint("status: ");
+	put_int(status, 10);
+	kprint("\n");
+
+	/*
 	if (ch)
 	{
-		put_char(ch);
 		if (keyboard_buffer)
 		{
 			if (ch == '\b')
-				keyboard_buffer_index--;
+			{
+				if (keyboard_buffer_index > 0)
+				{
+					keyboard_buffer_index--;
+					put_char(ch);
+				}
+			}
 			else {
 				keyboard_buffer[keyboard_buffer_index++] = ch;
+				put_char(ch);
 				if (ch == KEY_ENTER)
 					is_done = 1;
 			}
-		}
+		} else
+			put_char(ch);
 	}
+	*/
 }
 
 void gets(char *buffer, int length)
@@ -74,6 +118,8 @@ void gets(char *buffer, int length)
 
 	if (length < keyboard_buffer_index)
 		keyboard_buffer_index = length;
+
 	memcpy(buffer, keyboard_buffer, keyboard_buffer_index);
 	kfree(keyboard_buffer);
+	keyboard_buffer = 0;
 }
