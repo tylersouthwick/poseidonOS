6ea07ce9c0395dc182a5d84d1ef6e846ce7c557f
Tyler Southwick <tyler@northfuse.net>
2005-05-25 05:07:11 +0000

    *** empty log message ***
    git-svn-id: https://northfuse.net/svn/PoseidonOS/trunk@798 0e8f351e-ab8b-4634-bd77-693fb702483a

--- poseidonos/kernel/include/mm/virtual_mem.h
+++ poseidonos/kernel/include/mm/virtual_mem.h
@@ -2,6 +2,7 @@
 #define VIRTUAL_MEM_H
 
 void *mm_virtual_page_alloc(void);
+void *mm_virtual_page_alloc_multiple(int);
 void mm_virtual_page_free(void *);
 
 #endif
--- poseidonos/kernel/mm/virtual_mem.c
+++ poseidonos/kernel/mm/virtual_mem.c
@@ -39,6 +39,44 @@ void *mm_virtual_page_alloc() {
 	while(1);
 }
 
+/*********************************************************
+ * void *mm_virtual_page_alloc_multiple(int count)
+ *
+ * Allocates a count virtual pages
+ * *******************************************************/
+void *mm_virtual_page_alloc_multiple(int count) {
+				/*
+	unsigned long *current_pde, *temp_pte;
+	int pde_index, pte_index;
+
+	current_pde = (unsigned long*)((unsigned int)read_cr3() & 0xFFFFF000);
+
+	for(pde_index=0; pde_index<1024; pde_index++) {
+		if (current_pde[pde_index] & 1) {	//is it present?
+			temp_pte = (unsigned long*)((long)current_pde[pde_index] & 0xFFFFF000);
+			for(pte_index=0; pte_index<1024; pte_index++) {
+				if (!(temp_pte[pte_index] & 1)) {
+					temp_pte[pte_index] = (unsigned long)mm_physical_page_alloc(MM_TYPE_NORMAL);
+					temp_pte[pte_index] |= 3;
+					return (void *)((pde_index << 22) + (pte_index << 12));
+				}
+			}
+		}
+	}
+
+	kprint("ERROR: mm_virtual_page_alloc()");
+	asm volatile ("cli");
+	asm volatile ("hlt");
+	while(1);
+	*/
+	kprint("allocating ");
+	put_int(count, 10);
+	kprint(" pages...");
+
+	kprint("ok!\n");
+	return (void*)0;
+}
+
 void mm_virtual_page_free(void *v_addr) {
 	unsigned long *pde,*pte;
 
