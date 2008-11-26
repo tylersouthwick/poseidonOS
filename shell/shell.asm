b7380c6b991e82a6d4e25025bf2b7bbfec830962
Tyler Southwick <tyler@northfuse.net>
2008-11-26 18:24:07 +0000

    git-svn-id: https://northfuse.net/svn/PoseidonOS/trunk@1018 0e8f351e-ab8b-4634-bd77-693fb702483a

---
+++ shell/shell.asm
@@ -0,0 +1,103 @@
+start:
+	mov ebx, 10
+	int 45h
+	mov ebx, 10
+	int 45h
+	mov ebx, 'W'
+	int 45h
+	mov ebx, 'e'
+	int 45h
+	mov ebx, 'l'
+	int 45h
+	mov ebx, 'c'
+	int 45h
+	mov ebx, 'o'
+	int 45h
+	mov ebx, 'm'
+	int 45h
+	mov ebx, 'e'
+	int 45h
+	mov ebx, ' '
+	int 45h
+	mov ebx, 't'
+	int 45h
+	mov ebx, 'o'
+	int 45h
+	mov ebx, ' '
+	int 45h
+	mov ebx, 'P'
+	int 45h
+	mov ebx, 'o'
+	int 45h
+	mov ebx, 's'
+	int 45h
+	mov ebx, 'e'
+	int 45h
+	mov ebx, 'i'
+	int 45h
+	mov ebx, 'd'
+	int 45h
+	mov ebx, 'o'
+	int 45h
+	mov ebx, 'n'
+	int 45h
+	mov ebx, 'O'
+	int 45h
+	mov ebx, 'S'
+	int 45h
+	mov ebx, 10
+	int 45h
+	mov ebx, 's'
+	int 45h
+	mov ebx, 'h'
+	int 45h
+	mov ebx, 'e'
+	int 45h
+	mov ebx, 'l'
+	int 45h
+	mov ebx, 'l'
+	int 45h
+	mov ebx, '.'
+	int 45h
+	mov ebx, 'a'
+	int 45h
+	mov ebx, 'p'
+	int 45h
+	mov ebx, 'p'
+	int 45h
+	mov ebx, ' '
+	int 45h
+	mov ebx, '$'
+	int 45h
+	mov ebx, 10
+	int 45h
+	mov ebx, 'e'
+	int 45h
+	mov ebx, 'x'
+	int 45h
+	mov ebx, 'i'
+	int 45h
+	mov ebx, 't'
+	int 45h
+	mov ebx, 'i'
+	int 45h
+	mov ebx, 'n'
+	int 45h
+	mov ebx, 'g'
+	int 45h
+	mov ebx, ' '
+	int 45h
+	mov ebx, 's'
+	int 45h
+	mov ebx, 'h'
+	int 45h
+	mov ebx, 'e'
+	int 45h
+	mov ebx, 'l'
+	int 45h
+	mov ebx, 'l'
+	int 45h
+	mov ebx, 10
+	int 45h
+	ret
+
