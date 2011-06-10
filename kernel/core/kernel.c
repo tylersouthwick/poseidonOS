9d60cda2710eaa753c48dc97d5379477aeed5860
Tyler Southwick <tyler@northfuse.net>
2011-06-09 22:06:42 -0600

    made version a #define

--- kernel/core/kernel.c
+++ kernel/core/kernel.c
@@ -2,16 +2,18 @@
 #include <screen.h>
 #include <gdt.h>
 #include <idt.h>
+#include <version.h>
 
 void kmain() {
     initScreen();
     clear_screen();
-    INFO(("Booting PoseidonOS v1.0"));
+    INFO(("Booting PoseidonOS v%s", KERNEL_VERSION));
 
     idt_setup();
 
     gdt_init();
 
     enable_ints();
+
     while(1);
 }
