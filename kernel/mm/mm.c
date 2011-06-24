bc59fe13b251e188e8e19814464373fbf26c1df6
Tyler Southwick <tylersouthwick@byu.edu>
2011-06-24 09:13:19 -0600

    added virtual memory/paging

--- kernel/mm/mm.c
+++ kernel/mm/mm.c
@@ -2,9 +2,12 @@
 #include <mm/physical.h>
 #include <multiboot.h>
 #include <kdebug.h>
+#include <mm/paging.h>
 
 void mm_init(multiboot_info_t *mm_info) {
 	INFO(("Initializing Memory Management"));
 
 	mm_physical_init(mm_info);
+
+	mm_paging_init();
 }
