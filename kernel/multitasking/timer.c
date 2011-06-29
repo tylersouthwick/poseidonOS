48a566314854e484365e7e5469a967406e2a14df
Tyler Southwick <tylersouthwick@byu.edu>
2011-06-29 16:53:37 -0600

    changed pit interval

--- kernel/multitasking/timer.c
+++ kernel/multitasking/timer.c
@@ -5,7 +5,7 @@
 void timer_init() {
     DEBUG(("Initializing PIT"));
     idt_interrupt_add(0x20, timer_isr, 0);
-    struct PIT_RETURN *pitReturn = pit_init(1);
+    struct PIT_RETURN *pitReturn = pit_init(200);
     DEBUG(("PIT initialized %iHz every %i.%ims", pitReturn->irq0_frequency, pitReturn->irq0_ms, pitReturn->irq0_fractions));
 }
 
