06b74378695a2106cc254b5b6733b9181a8ea231
Tyler Southwick <tyler@northfuse.net>
2011-07-21 12:21:07 -0600

    added priority_queue

---
+++ kernel/include/util/priority_queue.h
@@ -0,0 +1,23 @@
+#ifndef MULTITTASKING_PRIORITY_QUEUE_H
+#define MULTITTASKING_PRIORITY_QUEUE_H
+
+typedef struct pq_node {
+	unsigned int data;
+	unsigned int priority;
+	struct pq_node *next, *prev;
+} pq_node;
+
+typedef struct priority_queue {
+	int count;
+	char *name;
+	pq_node *first, *last;
+} priority_queue;
+
+void priority_queue_create(priority_queue *queue, const char *name);
+void priority_queue_destroy(priority_queue * queue);
+void priority_queue_insert(priority_queue *queue, unsigned int data, unsigned int priority);
+int priority_queue_head(priority_queue *queue);
+void priority_queue_remove(priority_queue *queue, unsigned int data);
+void priority_queue_print(priority_queue *queue);
+
+#endif
---
+++ kernel/util/priority_queue.c
@@ -0,0 +1,156 @@
+#include <util/priority_queue.h>
+#include <kdebug.h>
+#include <string.h>
+#include <kmalloc.h>
+
+#define malloc(a) kmalloc(a)
+#define EMPTY_TASK -1
+
+static pq_node *create_node(unsigned int data, unsigned int priority) {
+	pq_node *node = malloc(sizeof(pq_node));
+	memset(node, 0, sizeof(pq_node));
+	node->data = data;
+	node->priority = priority;
+	return node;
+}
+
+void priority_queue_create(priority_queue *queue, const char *name) {
+	DEBUG(("Creating queue %s", name));
+	memset(queue, 0, sizeof(priority_queue));
+	queue->name = malloc(strlen(name) + 1);
+	strcpy(queue->name, name);
+	queue->count = 0;
+}
+
+void priority_queue_destroy(priority_queue * queue) {
+	pq_node *node;
+	DEBUG(("Destorying queue %s", queue->name));
+	kfree(queue->name);
+	node = queue->first;
+	while (node) {
+		pq_node *n = node->next;
+		kfree(node);
+		node = n;
+	}
+	queue->name = 0;
+	kfree(queue);
+}
+
+static int contains(priority_queue *queue, unsigned int data) {
+	int i = 0;
+	pq_node *n = queue->first;
+	while (n && i < queue->count) {
+		if (n->data == data) {
+			return 1;
+		}
+		n = n->next;
+	}
+	return 0;
+}
+
+static void insert_node(priority_queue *queue, pq_node *after, pq_node *n) {
+
+	n->next = after->next;
+	n->prev = after;
+
+	if (after->next)
+		after->next->prev = n;
+	else
+		queue->last = n;
+	after->next = n;
+}
+
+void priority_queue_insert(priority_queue *queue, unsigned int data, unsigned int priority) {
+	int i = 0;
+	pq_node *node;
+
+	if (queue->count == 0) {
+		DEBUG(("Inserting %i into queue[%s]", data, queue->name));
+		queue->first = queue->last = create_node(data, priority);
+		queue->count = 1;
+		return;
+	}
+
+	if (contains(queue, data)) {
+		DEBUG(("Queue %s already contains [%i]", queue->name, data));
+		return;
+	}
+
+	node = queue->first;
+	DEBUG(("iterating to insert %i into %s", data, queue->name));
+	while (node && i < queue->count) {
+		i++;
+		if (node->data == data) {
+			DEBUG(("%i already exists in queue [%s]", data, queue->name));
+			return;
+		}
+
+		if (priority <= node->priority) continue;
+
+		insert_node(queue, node, create_node(data, priority));
+		queue->count++;
+
+		return;
+	}
+
+	DEBUG(("Inserting %i at end of queue[%s]", data, queue->name));
+	{
+		pq_node *last = queue->last;
+		last->next = create_node(data, priority);
+		last->next->prev = last;
+		queue->last = last->next;
+	}
+	queue->count++;
+	return;
+}
+
+int priority_queue_head(priority_queue *queue) {
+	if (queue->count == 0) {
+		return -1;
+	}
+
+	DEBUG(("getting head from queue [%s]", queue-> name));
+
+	queue->count--;
+	pq_node *n = queue->first;
+	assert(n != NULL);
+	unsigned int data = n->data;
+	queue->first = n->next;
+	if (queue->first)
+		queue->first->prev = NULL;
+	if (n == queue->last)
+		queue->last = NULL;
+	kfree(n);
+
+	return data;
+}
+
+static void remove_node(priority_queue *queue, pq_node *n) {
+	DEBUG(("removing node..."));
+	if (n->prev)
+		n->prev->next = n->next;
+	else
+		queue->first = n->next;
+
+	if (n->next)
+		n->next->prev = n->prev;
+	else
+		queue->last = n->prev;
+}
+
+void priority_queue_remove(priority_queue *queue, unsigned int data) {
+	pq_node *n;
+	DEBUG(("Removing %i from queue [%s]", data, queue->name));
+	n = queue->first;
+	while (n) {
+		if (n->data != data) {
+			n = n->next;
+			continue;
+		}
+
+		//remove node
+		remove_node(queue, n);
+		queue->count--;
+		return;
+	}
+}
