53ef86382b039f0a8bf3e6b00764d470043fb04b
Tyler Southwick <tyler@northfuse.net>
2011-10-19 22:26:39 -0600

    use kprintf and made to work with testing

--- kernel/include/kdebug.h
+++ kernel/include/kdebug.h
@@ -1,17 +1,22 @@
 #ifndef KDEBUG_H
 #define KDEBUG_H
 
-#include <screen.h>
+#ifdef TEST
+	#include <stdio.h>
+	#define kprintf printf
+#else
+	#include <screen.h>
+#endif
 
 #define DEBUG(a) MESSAGE("DEBUG", a)
 
-#define INFO(a) kprintf a; write_char('\n')
+#define INFO(a) kprintf a; kprintf("\n")
 
 #define ERROR(a) MESSAGE("ERROR", a)
 
 #define FATAL(a) MESSAGE("FATAL", a)
 
-#define MESSAGE(t, a) kprintf("%s [%s:%i] ", t, __FILE__, __LINE__); kprintf a; write_char('\n')
+#define MESSAGE(t, a) kprintf("%s [%s:%i] ", t, __FILE__, __LINE__); kprintf a; kprintf("\n")
 
 #define assert(a) //TODO: implement
 #endif
