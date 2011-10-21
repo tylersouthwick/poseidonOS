86d74c9e17664ac03fc597f6852c741f12160da6
Tyler Southwick <tyler@northfuse.net>
2011-10-20 20:31:57 -0600

    added more test cases

--- kernel/util/priority_queue_test.c
+++ kernel/util/priority_queue_test.c
@@ -1,13 +1,25 @@
-#include "test.h"
+#include <test.h>
 #include <stdio.h>
 #include <string.h>
 #include <util/priority_queue.h>
 
-TESTCASE(empty_queue, {
-	priority_queue queue;
+#define QUEUE_TEST(testName, body) TESTCASE(testName, {priority_queue queue; priority_queue_create(&queue, FUNCTION_NAME(testName)); body})
 
-	priority_queue_create(&queue, "test1");
-	rc += test_assert_true(queue.size != 0);
+QUEUE_TEST(empty_queue, {
+	test_assert_true(queue.count == 0);
+});
+
+QUEUE_TEST(add_one_item, {
+	priority_queue_insert(&queue, 10, 5);
+	test_assert_true(queue.first != 0);
+	test_assert_true(queue.first->data == 10);
+	test_assert_true(queue.count == 1);
+});
+
+QUEUE_TEST(add_two_items, {
+	priority_queue_insert(&queue, 10, 5);
+	priority_queue_insert(&queue, 7, 10);
+	test_assert_true(queue.count == 2);
 });
 
 #define TEST_COUNT 25
@@ -18,6 +30,8 @@ int main(int argc, char **argv) {
 
 	int count = 0;
 	test_run(empty_queue, &results[count++]);
+	test_run(add_one_item, &results[count++]);
+	test_run(add_two_items, &results[count++]);
 
 	return test_summary(results, count);
 }
