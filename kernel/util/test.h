325edb46427eea824d9391fa6bab41e705ce5d47
Tyler Southwick <tyler@northfuse.net>
2011-10-20 20:20:58 -0600

    combined test.c and test.h

--- kernel/util/test.h
+++ kernel/util/test.h
@@ -3,26 +3,48 @@
 
 #include <kdebug.h>
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 struct test_result {
-	char name[255];
 	unsigned int success;
 	unsigned int failed;
 } typedef test_result_t;
 
-int test_summary(test_result_t *results, int count);
-void test_run(int (test)(test_result_t *result), test_result_t *result);
-
-static int test_assert_true(int expression) {
-	if (expression) {
-		return 0;
-	} else {
-		ERROR(("expression should be true"));
-		return 1;
-	}
-}
+static int test_summary(test_result_t *results, int count);
+static void test_run(int (test)(test_result_t *result), test_result_t *result);
 
 #define STR_VALUE(arg) #arg
 #define FUNCTION_NAME(name) STR_VALUE(name)
 
-#define TESTCASE(testName, body) static int testName (test_result_t *result) { int rc = 0; strcpy(result->name, FUNCTION_NAME(testName)); body; return rc;}
+#define TESTCASE(testName, body) static int testName (test_result_t *result) { int rc = 0; printf("%s ... ", FUNCTION_NAME(testName)); body; if (!rc) printf("SUCCESS\n"); return rc;}
+
+#define test_assert_true(expression) if (rc == 0) {if (!(expression)) {ERROR_MSG(("test should be true: %s", FUNCTION_NAME(expression))); rc++;}}
+
+static int test_summary(test_result_t *results, int count) {
+	int failed = 0;
+	int success = 0;
+	for (int i=0; i < count; i++) {
+		test_result_t *result = &results[i];
+		failed += result->failed;
+		success += result->success;
+	}
+
+	printf("SUCCESS: %i\nFAILURE: %i\n", success, failed);
+
+	return failed;
+}
+
+static void test_run(int (test)(test_result_t *result), test_result_t *result) {
+	int rc = test(result);
+
+	if (rc) {
+		result->failed = 1;
+		result->success = 0;
+	} else {
+		result->failed = 0;
+		result->success = 1;
+	}
+}
 #endif
