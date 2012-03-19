2e32ee6d010f506c6ee13a5538be250020692030
Tyler Southwick <tyler@northfuse.net>
2012-03-18 22:15:20 -0600

    updated debug messages

--- kernel/multitasking/tss.c
+++ kernel/multitasking/tss.c
@@ -33,6 +33,7 @@ void tss_init() {
 	DEBUG_MSG(("Creating KSTACK %i", KSTACK_SIZE));
 	system_tss.esp0 = kstackend = ((unsigned int) kernel_stack) + KSTACK_SIZE - 1000;
 	//system_tss.cr3 = read_cr3();
+	TRACE_MSG(("Created kstackend: %i", kstackend));
 	
 	DEBUG_MSG(("adding TSS to GDT"));
 	gdt_set_gate(GDT_ENTRY_TSS, (unsigned int) &system_tss, sizeof(tss_t), 0x89, 0x0F);
@@ -40,7 +41,7 @@ void tss_init() {
 	gdt_flush();
 
 	DEBUG_MSG(("loading tss"));
- 
 	__asm__ volatile ("ltr %%ax" : : "a" (GDT_ENTRY_TSS * sizeof(struct gdt_descriptor)) );
+	DEBUG_MSG(("loaded tss"));
 }
 
